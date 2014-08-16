cfa29441062d767621c9cf6619c80ace31701264
Jonathan <jonathan@Jonathans-MacBook-Air.local>
2014-08-15 19:38:42 -0700

    Configured grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -5,26 +5,49 @@ module.exports = function(grunt) {
 
 	// Project configuration
 	grunt.initConfig({
-	  jshint: {
-	    all: {
-	    	src: ['gruntfile.js', 'app.js','controllers/*.js', 'models/*.js', 'config/*.js']
-	  	}
-	  },
-	  nodemon: {
-	  	dev: {
-	  		script: 'server/app.js',
-	  		options: {
-	  			ext: 'js, html'
-	  		}
-	  	}
-	  }
+		// Starts FE server
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: ['client/dist', 'www-root']
+				}
+			}
+		},
+		// Runs jshint
+		jshint: {
+			all: {
+				src: ['gruntfile.js', 'app.js','controllers/*.js', 'models/*.js', 'config/*.js']
+			}
+		},
+		// Starts BE server
+		nodemon: {
+			dev: {
+				script: 'server/app.js',
+				options: {
+					ext: 'js, html'
+				}
+			}
+		}
 	});
 
-	// NPM tasks
+	// Load grunt tasks automatically
 	require('load-grunt-tasks')(grunt);
 
 	// Default to force
 	grunt.option('force', true);
 
-	grunt.registerTask('default', ['jshint', 'nodemon']);
+	grunt.registerTask('default', ['connect', 'jshint', 'nodemon']);
+
 };
+
+
+
+
+
+
+
+
+
+
+
